94b31e0bb8a16462c7dc59e99696fca90bb867d7
yusef7884 <64807950+yusef7884@users.noreply.github.com>
2020-08-03 01:58:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const sendBtn = document.querySelector('#sendBtn');
+const messageIn = document.querySelector('#messageIn');
+const messageOut = document.querySelector('#messageOut');
+
+sendBtn.addEventListener('click', sendMsg)
+
+function sendMsg() {
+    let content = messageIn.value;
+    if (content === ''){
+        alert('please Enter valid Value, Curent value is Empty')
+    }else{
+        messageOut.innerHTML = content;
+        messageIn.value = '';
+    }
+}
